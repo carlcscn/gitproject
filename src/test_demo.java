c44781184c42f12680b2983238ae07372f8f13f6
carlcscn <carlcs1994@gmail.com>
2018-09-11 20:44:21 +0800

    666ccc

--- src/test_demo.java
+++ src/test_demo.java
@@ -2,5 +2,7 @@ public class test_demo {
     public static void  main(String[] args){
         System.out.println("test_demo");
         System.out.println("test_demo1");
+        System.out.println("test_demo2");
+
     }
 }
